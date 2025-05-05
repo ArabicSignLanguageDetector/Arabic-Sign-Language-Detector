7e4158f5f2b1413e5bcfa9afa47c5464844bb619
ArabicSignLanguageDetector <projectrecognition44@gmail.com>
2025-05-05 15:19:55 +0300

    Update app4.py

--- app4.py
+++ app4.py
@@ -86,4 +86,5 @@ def video():
                 spoken = True
                 return label, 200
 
-    return '', 204
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
