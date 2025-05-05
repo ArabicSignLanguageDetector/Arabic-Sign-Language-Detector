cd97a6b94683590ee17972dbb5a349e0774fc777
ArabicSignLanguageDetector <projectrecognition44@gmail.com>
2025-05-05 13:54:43 +0300

    Update app4.py

--- app4.py
+++ app4.py
@@ -89,6 +89,8 @@ def video():
 
     return '', 204
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
+
 
